6b5761e031007198f086138f1f8eb38ea058dac0
admin <admin@CHETUIWK1330>
2022-03-16 14:17:16 +0530

    Add project files.

---
+++ RaviCurdApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using RaviCurdApp.DB_New;
+using RaviCurdApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RaviCurdApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Studentdatils obj)
+        {
+            STUDENTSINFOEntities res = new STUDENTSINFOEntities();
+
+            STUDETAIL obj1 = new STUDETAIL();
+
+            obj1.SNAME = obj.SNAME;
+            obj1.FEE = obj.FEE;
+            obj1.ROLL = obj.ROLL;
+
+            res.STUDETAILs.Add(obj1);
+            res.SaveChanges();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ RaviCurdApp/Models/Studentdatils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaviCurdApp.Models
+{
+    public class Studentdatils
+    {
+        public int SID { get; set; }
+        public string SNAME { get; set; }
+        public int FEE { get; set; }
+        public string ROLL { get; set; }
+    }
+}
